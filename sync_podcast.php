40d21be93805d1ae053b36e37c00621eb5d21d6f
Javier Malonda <jmalonda@gmail.com>
2023-01-23 15:55:18 +0100

    Changed post title format

--- sync_podcast.php
+++ sync_podcast.php
@@ -3,7 +3,7 @@
 Plugin Name: ESDLV Podcast syncronizer
 Plugin URI: ---
 Description: This plugin will syncronize the podcast feed with the ESDLV website.
-Version: 0.1
+Version: 0.2
 Author: Javier Malonda
 Author URI: ---
 License: GPL2
@@ -14,17 +14,35 @@ It reads the podcast feed and finds out if there are new podcast episodes.
 If so, it creates a new post entry for each episode.
 */
 
-
-add_action('rest_api_init', 'sync_podcast_register_route');
-
 // Register a route from which the code will be called
-// URL = http://localhost/wp-json/sync/podcast
+// URL = https://elsentidodelavida.net/wp-json/sync/podcast
 function sync_podcast_register_route() {
     register_rest_route('sync', 'podcast', array(
         'methods' => 'GET',
         'callback' => 'sync_podcast',
             )
     );
+}
+add_action('rest_api_init', 'sync_podcast_register_route');
+
+
+function prepare_post( $item, $episode_title ) {
+    // Create an iframe containing the episode audio player
+    $episode_link = $item->link;
+    $episode_ahref = "<a href='" . $episode_link . "'>" . $episode_title . "</a>";
+    // Rewrite the episode link adding '/embed' after 'esdlv'
+    $episode_link = str_replace("esdlv", "esdlv/embed", $episode_link);
+    $episode_iframe = "<div class='podcast'>
+        <iframe src='" . $episode_link . "' height='102px' width='463px' frameborder='0' scrolling='no'></iframe>
+        </div>";
+    // Set current user to admin
+    wp_set_current_user( 1 );
+    // Convert $item->pubDate to the format [ Y-m-d H:i:s ]
+    $episode_date = date("Y-m-d H:i:s", strtotime($item->pubDate));
+    $post_content = $item->description . "<br>" . $episode_iframe;
+    $post_info = array( $post_content, $episode_date );
+
+    return $post_info;
 }
 
 function sync_podcast() {
@@ -42,37 +60,14 @@ function sync_podcast() {
     $i = 0;
     foreach ($feed->channel->item as $item) {
         $episode_title = $item->title;
-        // Convert episode title format from "number. title" to "Podcast Ep. number: title"
-        $episode_title = "Podcast Ep. " . $episode_title;
-        // Convert only the . after the number to :
-        $episode_title = str_replace(".", ":", $episode_title);
-        // Replace Ep: with Ep.
-        $episode_title = str_replace("Ep:", "Ep.", $episode_title);
-
-        echo "\n Episode title: " . $episode_title;
-
         $episode_post = get_page_by_title($episode_title, OBJECT, 'post');
         echo "\n Episode id: " . $episode_post->ID;
 
-        // Create an iframe containing the episode audio player
-        $episode_link = $item->link;
-        $episode_ahref = "<a href='" . $episode_link . "'>" . $episode_title . "</a>";
-        // Rewrite the episode link adding '/embed' after 'esdlv'
-        $episode_link = str_replace("esdlv", "esdlv/embed", $episode_link);
-        $episode_iframe = "<div class='podcast'>
-            <iframe src='" . $episode_link . "' height='102px' width='463px' frameborder='0' scrolling='no'></iframe>
-            </div>";
-
-        echo "\n Episode iframe: " . $episode_iframe . "\n";
-
-        wp_set_current_user( 1 );
-
-        // Convert $item->pubDate to the format [ Y-m-d H:i:s ]
-        $episode_date = date("Y-m-d H:i:s", strtotime($item->pubDate));
-
-        $post_content = $item->description . "<br>" . $episode_iframe;
-        
-        if (!$episode_post) {
+        // Version 0.2 after post_id: 4597
+        if ( !$episode_post && $episode_post->ID > 4597 ) {
+            $post_info = prepare_post( $item );
+            $post_content = $post_info[0];
+            $post_date = $post_info[1];
             // Create a new post entry
             $post_id = wp_insert_post(array(
                 'post_title' => $episode_title,
@@ -83,7 +78,7 @@ function sync_podcast() {
                 // Category id for podcast = 351
                 'post_category' => array(351),
                 // Post date is the podcast publication date
-                'post_date' => $episode_date
+                'post_date' => $post_date
             ));
 
             // Echo the post id
